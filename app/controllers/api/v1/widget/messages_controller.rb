8300aed535bf18599a0350c8b485473c84c02ff8
Pooja Chhillar <poojachhillar111@example.com>
2025-07-17 10:48:32 +0530

    send the static message to incoming message in widget inbox

--- app/controllers/api/v1/widget/messages_controller.rb
+++ app/controllers/api/v1/widget/messages_controller.rb
@@ -10,6 +10,14 @@ class Api::V1::Widget::MessagesController < Api::V1::Widget::BaseController
     @message = conversation.messages.new(message_params)
     build_attachment
     @message.save!
+    return unless @message.incoming?
+
+    conversation.messages.create!(
+      content: 'Thank you for your message. Our team will reply shortly.',
+      message_type: 'outgoing',
+      account_id: conversation.account_id,
+      inbox_id: conversation.inbox_id
+    )
   end
 
   def update
